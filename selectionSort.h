10942597301f8bcccbf1295907b17b9ecd8b427b
Ray Lauff <ray@temple.edu>
2013-03-21 09:48:46 -0400

    Added more comments

--- selectionSort.h
+++ selectionSort.h
@@ -7,11 +7,18 @@
 
 
 // prototypes
+
+// public prototyes
 void sort_array( int a[], int n, char sortorder );
+void chomp( char a[] );
+void skip_file_input_line( FILE *IN );
+
+// private prototypes
 int  get_min_range( int a[], int first, int last );
 int  get_max_range( int a[], int first, int last );
 
 
+// find the smallest item in the array
 int get_min_range( int a[], int first, int last )
 {
 	int i = first;
@@ -24,8 +31,7 @@ int get_min_range( int a[], int first, int last )
 	return smallest;
 }
 
-
-
+// find the largest item in the array
 int get_max_range( int a[], int first, int last )
 {
 	int i = first;
@@ -38,42 +44,52 @@ int get_max_range( int a[], int first, int last )
 	return largest;
 }
 
+// this function can be used instead of get_min_range or get_max_range.
+int find_item_to_swap( int a[], char sortorder, int first, int last )
+{
+	int i = first;
+	int swapitem = first;
+	
+	if ( sortorder == SORT_ASCENDING )
+		for ( i = first + 1; i <= last; i++ ) {
+			if ( a[ i ] > a[ swapitem ] )
+				swapitem = i;
+		}
+	else if ( sortorder == SORT_DESCENDING ) 
+		for ( i = first + 1; i <= last; i++ ) {
+			if ( a[ i ] < a[ swapitem ] )
+				swapitem = i;
+		}
+	else {
+		puts( "Invalid sorting order passed to function find_item_to_swap( )." );
+		exit( EXIT_FAILURE );
+	}
+		    
+	return swapitem;
+}
+
 void sort_array( int a[], int n, char ordering )
 {
 	int fill, temp, index_of_min;
+
+	if ( ordering != SORT_ASCENDING && ordering != SORT_DESCENDING ) {
+		printf( "Invalid sort ordering passed to function sort_array.  Value=%c.\n", ordering );
+		printf( "Must be %c or %c.\n", SORT_ASCENDING, SORT_DESCENDING );
+		exit( EXIT_FAILURE );
+	}
 	
-	if ( ordering == SORT_ASCENDING ) {
-	    for ( fill = 0; fill < n - 1; ++fill ) {
-		    index_of_min = get_min_range( a, fill, n - 1 );
+	for ( fill = 0; fill < n - 1; ++fill ) {
+	    index_of_min = find_item_to_swap( a, ordering, fill, n - 1 );
 
-		    if ( fill != index_of_min ) {
-			    temp = a[ index_of_min ];
-			    a[ index_of_min ] = a[ fill ];
-			    a[ fill ] = temp;
-		    }
+	    if ( fill != index_of_min ) {
+		    temp = a[ index_of_min ];
+		    a[ index_of_min ] = a[ fill ];
+		    a[ fill ] = temp;
 	    }
 	} 
-
-	else 
-	if ( ordering == SORT_DESCENDING ) {
-	    for ( fill = 0; fill < n - 1; ++fill ) {
-		    index_of_min = get_max_range( a, fill, n - 1 );
-
-		    if ( fill != index_of_min ) {
-			    temp = a[ index_of_min ];
-			    a[ index_of_min ] = a[ fill ];
-			    a[ fill ] = temp;
-		    }
-	    }
-	} 
-
-	else
-		printf( "Invalid sort order passed: \"%c\".  Must be %c or %c.\n", ordering, SORT_ASCENDING, SORT_DESCENDING );
 
 	return;
 }
-
-
 
 
 
